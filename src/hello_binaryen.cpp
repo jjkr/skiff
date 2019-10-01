0144602869ba23447c46df4ac9c7bf7bc26b56f8
Joe Kramer <joe.kramer.cu@gmail.com>
2019-10-01 04:43:56 +0000

    Fix binaryen

--- src/hello_binaryen.cpp
+++ src/hello_binaryen.cpp
@@ -3,6 +3,29 @@
 int main()
 {
     BinaryenModuleRef module = BinaryenModuleCreate();
+
+    BinaryenType paramTypes[] = { BinaryenTypeInt32() };
+    BinaryenFunctionTypeRef addFiveType = BinaryenAddFunctionType(
+            module,
+            "addFiveType",
+            BinaryenTypeInt32(),
+            paramTypes,
+            1);
+
+    BinaryenExpressionRef expr = BinaryenBinary(
+            module,
+            BinaryenAddInt32(),
+            BinaryenLocalGet(module, 0, BinaryenTypeInt32()),
+            BinaryenConst(module, BinaryenLiteralInt32(5)));
+    BinaryenType varTypes[] = {};
+    BinaryenAddFunction(
+            module,
+            "addFive",
+            addFiveType,
+            varTypes,
+            0,
+            expr);
+
     BinaryenModulePrint(module);
     BinaryenModuleDispose(module);
 }
